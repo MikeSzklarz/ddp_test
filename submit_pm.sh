424d6a8da56dd2cc6959af2c0732dfe02c4551df
mszklarz <mike.szklarz@gmail.com>
2025-01-28 13:14:42 -0500

    Initial Commit

---
+++ submit_pm.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+#SBATCH --job-name=vit-era5_mn_%j                   # Job name
+#SBATCH --output=logs/slurm/job_%j.txt              # Output log
+#SBATCH --nodes=1                                   # Number of nodes
+#SBATCH --ntasks-per-node=1                         # Number of tasks to invoke on each node
+#SBATCH --mem=65536                                 # Memory (64 GB)
+#SBATCH --time=30-00:00:00                          # Job time limit
+#SBATCH --partition=waccamaw                        # Partition to use
+#SBATCH --exclusive                                 # Exclusive node allocation
+#SBATCH --exclude=waccamaw03,waccamaw04             # Exclude specific nodes
+
+LOGDIR=${PWD}/logs
+mkdir -p ${LOGDIR}
+args="${@}"
+
+# No Errors dont need extra file
+source /mnt/cidstore1/software/debian12/anaconda3/etc/profile.d/conda.sh
+conda activate test_ddp
+
+export FI_MR_CACHE_MONITOR=userfaultfd
+export HDF5_USE_FILE_LOCKING=FALSE
+
+export MASTER_ADDR=$(hostname)
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+export WORLD_SIZE=$SLURM_NTASKS
+export MASTER_PORT=29500 # default from torch launcher
+
+export NCCL_DEBUG=INFO
+export TORCH_CPP_LOG_LEVEL=INFO
+export TORCH_DEISTRIBUTED_DEBUG=INFO
+
+# Having multiple gpus available when only one is avaiable causes issues
+export CUDA_VISIBLE_DEVICES=0
+
+# Debugging mode
+set -x
+
+# Run the training script directly
+srun -u bash -c "
+    ${PROFILE_CMD} python train.py ${args}
+"
